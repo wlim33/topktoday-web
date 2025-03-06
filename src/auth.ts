e8d9bbc3c3a809877b45379d3d061de40da7dbb1
William Lim <limwilliam23@gmail.com>
2025-03-06 01:29:25 -0500

    fix: package issues

--- src/auth.ts
+++ src/auth.ts
@@ -3,9 +3,10 @@ import { username } from "better-auth/plugins"
 import { anonymous } from "better-auth/plugins"
 import { usernameSchema } from "@/lib/server/schemas";
 import { linkAnonymousAccount } from "@/lib/server/api";
-import pg from "node:pg";
+import * as pg from 'pg'
+const { Pool } = pg
 
-export const db = new pg.Pool({
+export const db = new Pool({
 	connectionString: import.meta.env.DB_URL
 })
 

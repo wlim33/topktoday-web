1f9bcf2ac86233ce21996ecf51fea9c5f12d4f70
William Lim <limwilliam23@gmail.com>
2025-03-06 01:16:24 -0500

    fix: package issuse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import netlify from '@astrojs/netlify/functions';
 
 
 export default defineConfig({
   output: 'server',
-  //vite: {
-  //  plugins
-  //},
+  vite: {
+    plugins: [nodePolyfills()]
+  },
   adapter: netlify({ edgeMiddleware: true }),
 
 })
